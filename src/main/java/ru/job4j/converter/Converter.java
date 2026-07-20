c28c308ea57f820f1b57b02d8a3fbcce76c0fe30
Kageelee <kirill8102@mail.ru>
2026-07-20 20:01:32 +0500

    1. Модульные тесты [#505277 #565921]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -5,6 +5,7 @@ public class Converter {
     public static double rubleToEuro(double value) {
         return value / 90;
     }
+
     public static double rubleToDollar(double value) {
         return value / 80;
     }
