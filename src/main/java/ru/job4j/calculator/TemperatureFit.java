f75e89abc26c2a41fb95fdb0359cb0b7e9f288e2
Kageelee <kirill8102@mail.ru>
2026-07-22 14:47:48 +0500

    Тестирование Класса Калькулятора Температуры Хранения [#505279]

---
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculator;
+
+public class TemperatureFit {
+
+    public static double idealTemperatureForFruits(int temperature) {
+        return temperature - 10.0;
+    }
+
+    public static double idealTemperatureForMeat(int temperature) {
+        return temperature - 30.0;
+    }
+}
---
+++ src/test/java/ru/job4j/calculator/TemperatureFitTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class TemperatureFitTest {
+
+    @Test
+    void whenRoom25ThenFruits15() {
+        int input = 25;
+        double expected = 15.0;
+        double output = TemperatureFit.idealTemperatureForFruits(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenRoom30ThenMeat0() {
+        int input = 30;
+        double expected = 0.0;
+        double output = TemperatureFit.idealTemperatureForMeat(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
