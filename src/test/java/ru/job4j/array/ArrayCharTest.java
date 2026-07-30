c3722d6f533bc0a4471e384c477cf8205264deb0
Kageelee <kirill8102@mail.ru>
2026-07-30 13:50:00 +0500

    Слово начинается с ... [#505233]

---
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testWordHellixTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'i', 'x'};
+        char[] prefix = {'H', 'e', 'l', 'l', 'i', 'x'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testWordHellixFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'i', 'x'};
+        char[] prefix = {'H', 'e', 'l', 'l', 'i', 'd'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isFalse();
+    }
+}
